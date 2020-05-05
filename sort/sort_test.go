68c2f1b80f417162b2bb9449d0c2c842ac74c87b
Sunil Pandey <64631561+gsbp@users.noreply.github.com>
2020-05-05 11:46:02 -0700

    Add more unit tests for sort algorithms

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -7,9 +7,19 @@ import (
 	"time"
 )
 
-func randomArray(size int) []int {
+func randomRange(size int) []int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return r.Perm(size)
+}
+
+func randomArray(size, max int) []int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	a := make([]int, size)
+	for i := range(a) {
+		a[i] = r.Intn(max)
+	}
+	return a
 }
 
 type SortFunc func([]int)
@@ -22,11 +32,62 @@ var algos = map[string]SortFunc {
 	"Selection sort": SelectionSort,
 }
 
-func TestSort(t *testing.T) {
+func runTest(t *testing.T, name string, fn SortFunc, a []int) {
+	fn(a)
+	if !sort.IntsAreSorted(a) {
+		t.Error(name, "returned:", a)
+	}
+}
+
+func TestSortRandom(t *testing.T) {
 	for name, fn := range algos {
-		a := randomArray(100)
+		a := randomArray(100, 100)
+		runTest(t, name, fn, a)
+	}
+}
+
+func TestSortRandomDistinct(t *testing.T) {
+	for name, fn := range algos {
+		a := randomRange(100)
+		runTest(t, name, fn, a)
+	}
+}
+
+func TestSortAllDuplicates(t *testing.T) {
+	for name, fn := range algos {
+		a := make([]int, 100)
+		for i := range(a) {
+			a[i] = 42
+		}
+		runTest(t, name, fn, a)
+	}
+}
+
+func TestSortAlreadySorted(t *testing.T) {
+	for name, fn := range algos {
+		a := make([]int, 100)
+		for i := range(a) {
+			a[i] = i
+		}
+		runTest(t, name, fn, a)
+	}
+}
+
+func TestSortAlreadySortedDescending(t *testing.T) {
+	for name, fn := range algos {
+		a := make([]int, 100)
+		for i := range(a) {
+			a[i] = 99 - i
+		}
+		runTest(t, name, fn, a)
+	}
+}
+
+func TestSortEmpty(t *testing.T) {
+	for name, fn := range algos {
+		a := []int{}
 		fn(a)
-		if !sort.IntsAreSorted(a) {
+		if len(a) != 0 {
 			t.Error(name, "returned:", a)
 		}
 	}
